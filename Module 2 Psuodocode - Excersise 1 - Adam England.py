359c87554f568c47b44f43fd4b50d149dc9c706f
englanaw <aengland51@ivytech.edu>
2024-11-02 15:18:30 -0400

    Add files via upload

---
+++ Module 2 Psuodocode - Excersise 1 - Adam England.py
@@ -0,0 +1,22 @@
+# psudocode Exercise 1
+
+# Start
+
+# Inputs
+
+# input refrigerator Model
+# input Width in inches     
+# input Height in inches
+# input Depth in inches 
+
+# Process
+
+# cubicInches = Height * Width * Depth
+# cubicFeet = cubicinches / 1728
+
+# Outputs
+
+# output cubicfeet
+# output refrigerator Model
+
+#Stop 
